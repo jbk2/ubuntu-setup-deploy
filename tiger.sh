f086834e4990eadce52f748a39bf77713f10e18b
jbk2 <james@humanbenefits.com>
2024-06-13 17:56:50 +0200

    first commit - Installed Nginx, serving index.html via local script file

---
+++ tiger.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USER=ubuntu
+SERVER=$SERVER
+PORT=22 # default ssh port
+SSH_KEY=$SSH_KEY
+SSH_OPTIONS="-i $SSH_KEY -o StrictHostKeyChecking=no"
+SSH_ARGS="$USER@$SERVER -p $PORT $SSH_OPTIONS"
+SCP_ARGS="-i $SSH_KEY"
+
+#Debug output
+echo "Connecting to $USER@$SERVER on port $PORT using SSH key $SSH_KEY"
+
+ssh $SSH_ARGS 'bash -s' <<-STDIN
+# set bash session options to catch errors
+set -euo pipefail
+
+# Install nginx
+sudo apt install -y nginx
+STDIN
+
+DIR=$(dirname "$(realpath "$0")")
+echo " here's the directory we're in and are copying index from; $DIR"
+scp $SCP_ARGS $DIR/index.html $USER@$SERVER:~/index.html && echo "successfully copied ~/index to server"
+
+ssh $SSH_ARGS 'bash -s' <<-STDIN
+  set -euo pipefail
+  # Move the file to the nginx html directory
+  sudo mv ~/index.html /var/www/html/index.html && echo "successfully moved ~/index to /var/www/html/"
+  sudo systemctl restart nginx.service && echo "successfully restarted nginx"
+STDIN
